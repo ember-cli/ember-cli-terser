22e596b64a679ef584020db485869e1e3a54702b
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-07-09 17:53:03 +0200

    Update "broccoli-uglify-sourcemap" to v2.0.0-beta.1

--- index.js
+++ index.js
@@ -1,18 +1,53 @@
-/* jshint node: true */
 'use strict';
 
 module.exports = {
   name: 'ember-cli-uglify',
+
   included: function(app) {
     this._super.included.apply(this,arguments);
 
-    this.options = app.options;
+    const defaults = require('lodash.defaultsdeep');
+
+    let defaultOptions = {
+      enabled: app.env === 'production',
+
+      uglify: {
+        compress: {
+          // this is adversely affects heuristics for IIFE eval
+          'negate_iife': false,
+          // limit sequences because of memory issues during parsing
+          sequences: 30,
+        },
+        output: {
+          // no difference in size and much easier to debug
+          semicolons: false,
+        },
+      }
+    };
+
+    if (app.options.sourcemaps && !this._sourceMapsEnabled(app.options.sourcemaps)) {
+      defaultOptions.uglify.sourceMap = false;
+    }
+
+    this._options = defaults(app.options['ember-cli-uglify'] || {}, defaultOptions);
   },
+
+  _sourceMapsEnabled(options) {
+    if (options.enabled === false) {
+      return false;
+    }
+
+    let extensions = options.extensions || [];
+    if (extensions.indexOf('js') === -1) {
+      return false;
+    }
+
+    return true;
+  },
+
   postprocessTree: function(type, tree) {
-    if(this.options.minifyJS.enabled === true && type === 'all') {
-      var options = this.options.minifyJS.options || {};
-      options.sourceMapConfig = this.options.sourcemaps;
-      return require('broccoli-uglify-sourcemap')(tree, options);
+    if (this._options.enabled === true && type === 'all') {
+      return require('broccoli-uglify-sourcemap')(tree, this._options);
     } else {
       return tree;
     }
