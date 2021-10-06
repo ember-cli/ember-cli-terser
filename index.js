9044eab3682f3d0ea25a9c46b78cd5e6655b0ae9
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-10-05 20:12:19 -0400

    chore: replace a bunch of stuff with esbuild

--- index.js
+++ index.js
@@ -9,58 +9,39 @@ module.exports = {
     let defaultOptions = {
       enabled: app.env === 'production',
 
-      terser: {
-        compress: {
-          // this is adversely affects heuristics for IIFE eval
-          'negate_iife': false,
-          // limit sequences because of memory issues during parsing
-          sequences: 30,
-        },
-        mangle: {
-          safari10: true
-        },
-        output: {
-          // no difference in size and much easier to debug
-          semicolons: false,
-        },
-      }
+      esbuild: {}
     };
 
-    if (app.options.sourcemaps && !this._sourceMapsEnabled(app.options.sourcemaps)) {
-      defaultOptions.terser.sourceMap = false;
+    if (app.options.sourcemaps && !areSourceMapsEnabled(app.options.sourcemaps)) {
+      defaultOptions.esbuild.sourceMap = false;
     }
 
-    let addonOptions = app.options['ember-cli-terser'];
+    let addonOptions = app.options['ember-cli-esbuild'];
 
-    if ('ember-cli-uglify' in app.options) {
-      this.ui.writeWarnLine('[ember-cli-terser] Passing options as `ember-cli-uglify` in `ember-cli-build.js` is deprecated, please update to passing `ember-cli-terser` (with a `terser` property) instead.');
-
-      addonOptions = Object.assign({}, app.options['ember-cli-uglify'], { terser: app.options['ember-cli-uglify'].uglify, uglify: undefined });
-    }
-
-    this._terserOptions = Object.assign({}, defaultOptions, addonOptions);
-  },
-
-  _sourceMapsEnabled(options) {
-    if (options.enabled === false) {
-      return false;
-    }
-
-    let extensions = options.extensions || [];
-    if (extensions.indexOf('js') === -1) {
-      return false;
-    }
-
-    return true;
+    this._esbuildOptions = Object.assign({}, defaultOptions, addonOptions);
   },
 
   postprocessTree(type, tree) {
-    if (this._terserOptions.enabled === true && type === 'all') {
-      const Terser = require('broccoli-terser-sourcemap');
+    if (this._esbuildOptions.enabled === true && type === 'all') {
+      const ESBuildPlugin = require('./lib');
 
-      return new Terser(tree, this._terserOptions);
+      return new ESBuildPlugin(tree, this._esbuildOptions);
     } else {
       return tree;
     }
   }
 };
+
+function areSourceMapsEnabled(options) {
+  if (options.enabled === false) {
+    return false;
+  }
+
+  let extensions = options.extensions || [];
+  if (extensions.indexOf('js') === -1) {
+    return false;
+  }
+
+  return true;
+
+}
