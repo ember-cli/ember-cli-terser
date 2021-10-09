5442a3f8fa5b2b29a1f856017bde2c568f9c7795
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-10-09 17:39:58 -0400

    chore: fix volta warning, add some JS with decorators

---
+++ tests/ember-app/app/components/button.js
@@ -0,0 +1,11 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+
+export default class ButtonComponent extends Component {
+  @tracked wasClicked = false;
+
+  @action click() {
+    this.wasClicked = true;
+  }
+}
