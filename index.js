f0eae165b980ab0b732f11899264cc99c7ad1fbd
Bert De Block <bert.deblock@bagaar.be>
2023-02-11 13:36:17 +0100

    Remove support for `ember-cli-uglify` config object

--- index.js
+++ index.js
@@ -6,20 +6,12 @@ module.exports = {
   included(app) {
     this._super.included.apply(this, arguments);
 
-    let addonOptions = app.options['ember-cli-terser'];
-
-    if ('ember-cli-uglify' in app.options) {
-      this.ui.writeWarnLine('[ember-cli-terser] Passing options as `ember-cli-uglify` in `ember-cli-build.js` is deprecated, please update to passing `ember-cli-terser` (with a `terser` property) instead.');
-
-      addonOptions = Object.assign({}, app.options['ember-cli-uglify'], { terser: app.options['ember-cli-uglify'].uglify, uglify: undefined });
-    }
-
     const { buildTerserOptions } = require('./lib/build-terser-options');
 
     this._terserOptions = buildTerserOptions({
       appEnv: app.env,
       appSourceMaps: app.options.sourcemaps,
-      userOptions: addonOptions,
+      userOptions: app.options['ember-cli-terser'],
     });
   },
 
